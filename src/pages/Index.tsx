6c472975c12ce16879d696e0bb1e907dc43137c2
EastDigital <eastdigitalcompany@gmail.com>
2025-06-07 17:19:01 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Navbar from '@/components/Navbar/Navbar';
 import { Hero } from '@/components/Hero/Hero';
@@ -6,10 +5,18 @@ import { VisionToRealitySection } from '@/components/VisionToReality/VisionToRea
 import { AboutSection } from '@/components/About/AboutSection';
 import { IndustrySection } from '@/components/Industry/IndustrySection';
 import Footer from '@/components/Footer/Footer';
+import AuroraBackground from '@/components/AuroraBackground';
+import { useNavbarLogic } from '@/components/Navbar/useNavbarLogic'; // We need the hook here now
 
 const Index = () => {
+  // Logic to determine if the aurora should be visible
+  const { isScrolled, isHomePage } = useNavbarLogic();
+  const showAurora = isHomePage && !isScrolled;
+
   return (
-    <div className="min-h-screen flex flex-col bg-eastdigital-dark">
+    // The main container needs `isolate` to create a new stacking context
+    <div className="min-h-screen flex flex-col bg-eastdigital-dark relative isolate">
+      <AuroraBackground isVisible={showAurora} />
       <Navbar />
       <Hero />
       <VisionToRealitySection />
