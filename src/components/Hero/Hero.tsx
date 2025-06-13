bad0a10ea7f3aee47e07a52a0a3d5ed366f82e33
EastDigital <eastdigitalcompany@gmail.com>
2025-06-13 11:23:25 +0530

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,12 @@
-
 import React from 'react';
 import { HeroContent } from './HeroContent';
 import { ProjectCarousel } from './ProjectCarousel';
+import AuroraBackground from '../common/AuroraBackground'; // Added this import
 
 export const Hero = () => {
   return (
     <section className="relative min-h-screen bg-black overflow-hidden flex flex-col mx-0 pb-0 py-[100px] my-0">
+      <AuroraBackground /> {/* Added this component */}
       <div className="relative z-20 container mx-auto px-2 sm:px-4 flex flex-col justify-between min-h-[calc(40vh-30px)] sm:min-h-[calc(40vh-30px)]">
         <div className="pt-[60px] pb-0 py-0">
           <HeroContent />
