fa83265c7be26f9a7f9b4d1a138db2bfacd709ec
EastDigital <eastdigitalcompany@gmail.com>
2025-07-17 23:49:13 +0530

    Update LiquidGlassContainer.tsx

--- src/components/LiquidGlass/LiquidGlassContainer.tsx
+++ src/components/LiquidGlass/LiquidGlassContainer.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { cn } from '@/lib/utils';
 
@@ -15,15 +14,7 @@ export const LiquidGlassContainer = ({
 }: LiquidGlassContainerProps) => {
   return (
     <div className={cn("relative", className)}>
-      {/* Pure deep black background */}
-      <div className="absolute inset-0 bg-black">
-        {/* Very subtle ambient lighting - no harsh orbs */}
-        <div className="absolute top-1/3 left-1/4 w-96 h-96 bg-eastdigital-orange/[0.02] rounded-full blur-[120px] opacity-60"></div>
-        <div className="absolute bottom-1/3 right-1/4 w-80 h-80 bg-white/[0.008] rounded-full blur-[100px] opacity-40"></div>
-        
-        {/* Minimal atmospheric gradient */}
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-zinc-950/20"></div>
-      </div>
+      {/* The solid black background has been removed to reveal the styles passed to the container */}
       
       {/* Content */}
       <div className="relative z-10">
