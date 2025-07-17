a8080469103eae2b2440f899463794cd5f37e913
EastDigital <eastdigitalcompany@gmail.com>
2025-07-17 23:56:32 +0530

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { HeroContent } from './HeroContent';
 import { ProjectCarousel } from './ProjectCarousel';
 import { AuroraBackground } from '../common/AuroraBackground';
+
 export const Hero = () => {
-  return <section className="relative min-h-screen overflow-hidden flex flex-col mx-0 pb-0 py-[100px] my-0">
+  return (
+    <section className="relative min-h-screen overflow-hidden flex flex-col mx-0 pb-0 py-[100px] my-0">
       {/* Aurora effect constrained to upper half */}
       <div className="absolute top-0 left-0 right-0 h-[50vh] overflow-hidden z-0">
         <AuroraBackground className="absolute inset-0" />
@@ -20,6 +22,8 @@ export const Hero = () => {
       <div className="relative z-20 mt-4 sm:flex-1 sm:flex sm:items-end sm:mt-0 sm:pt-2">
         <ProjectCarousel />
       </div>
-    </section>;
+    </section>
+  );
 };
+
 export default Hero;
