c7aea07c89761b94d591cf86cb8c03ea227b2a51
EastDigital <eastdigitalcompany@gmail.com>
2025-07-19 11:10:41 +0530

    Update HeroContent.tsx

--- src/components/Hero/HeroContent.tsx
+++ src/components/Hero/HeroContent.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 export const HeroContent = () => {
   return <div className="text-left sm:text-center max-w-5xl -bottom-0 -bottom-0 mx-[0px]">
-      <p className="text-base sm:text-lg font-poppins font-thin tracking-[1px] py-0" style={{ color: '#eea849' }}>
+      <p className="text-base sm:text-lg font-poppins font-thin tracking-[1px] py-0" style={{ color: '#FFD194' }}>
         We Create Stories That Sell Spaces
       </p>
       
       <h1 
-        className="text-[22px] sm:text-[28px] font-poppins font-bold bg-gradient-to-b from-[#f46b45] to-[#eea849] bg-clip-text text-transparent mt-3 sm:mt-4 md:mt-6 mb-4 sm:mb-6 md:mb-8"
+        className="text-[22px] sm:text-[28px] font-poppins font-bold bg-gradient-to-b from-[#f46b45] to-[#FFD194] bg-clip-text text-transparent mt-3 sm:mt-4 md:mt-6 mb-4 sm:mb-6 md:mb-8"
         style={{ lineHeight: '26px' }}
       >
         Story-driven marketing <br />
@@ -16,7 +16,7 @@ export const HeroContent = () => {
       
       <p 
         className="text-sm sm:text-base max-w-3xl sm:mx-auto mb-3 sm:mb-8 md:mb-8 font-poppins font-thin leading-relaxed py-0"
-        style={{ color: '#eea849' }}
+        style={{ color: '#FFD194' }}
       >
         We help developers, architects, and engineers accelerate sales through <br />
         high-impact 3d visuals, videos, branding, and digital campaigns.
