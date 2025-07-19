c569febac142cef5dfc35d77bebe826706a80ef9
EastDigital <eastdigitalcompany@gmail.com>
2025-07-19 12:13:28 +0530

    Update HeroContent.tsx

--- src/components/Hero/HeroContent.tsx
+++ src/components/Hero/HeroContent.tsx
@@ -4,7 +4,7 @@ export const HeroContent = () => {
   return <div className="text-left sm:text-center max-w-5xl -bottom-0 -bottom-0 mx-[0px]">
       
       
-      <h1 className="text-[26px] sm:text-[36px] font-poppins font-bold bg-gradient-to-b from-[orange] to-[#fadccd] bg-clip-text text-transparent mt-3 sm:mt-4 md:mt-6 mb-4 sm:mb-6 md:mb-8" style={{
+      <h1 className="text-[26px] sm:text-[36px] font-poppins font-bold bg-gradient-to-b from-[white] to-[offwhite] bg-clip-text text-transparent mt-3 sm:mt-4 md:mt-6 mb-4 sm:mb-6 md:mb-8" style={{
       lineHeight: '42px'
     }}>
         We Create Stories That Sell Spaces
