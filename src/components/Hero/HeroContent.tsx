ab00193d7464c92a6b8bf9f6f73fafa3e34a99d4
EastDigital <eastdigitalcompany@gmail.com>
2025-07-19 12:19:19 +0530

    Update HeroContent.tsx

--- src/components/Hero/HeroContent.tsx
+++ src/components/Hero/HeroContent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 export const HeroContent = () => {
-  return <div className="text-left sm:text-center max-w-5xl -bottom-0 -bottom-0 mx-[0px]">
+  return <div className="text-left sm:text-center max-w-3xl -bottom-0 -bottom-0 mx-[0px]">
       
       
       <h1 className="text-[26px] sm:text-[36px] font-poppins font-thin bg-gradient-to-b from-[white] to-[beige] bg-clip-text text-transparent mt-3 sm:mt-4 md:mt-6 mb-4 sm:mb-6 md:mb-8" style={{
